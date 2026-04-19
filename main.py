a9b8a3f8391ec4b3a798e31db76e6af4b5310a4c
SANATH200525 <1NT23IS192.Sanath@nmit.ac.in>
2026-04-19 15:36:56 +0530

    resolved cors error

--- main.py
+++ main.py
@@ -20,13 +20,13 @@ app = FastAPI()
 app.mount("/frontend", StaticFiles(directory="frontend"), name="frontend")
 
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
